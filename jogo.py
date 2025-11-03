f59e9382af06f9c2fc4ad97b8711dc2fd0cb0187
nathaly-creator <nathaly.gabriellegoncalvesda.silva@escola.pr.gov.br>
2025-11-03 09:47:42 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,1 +1,38 @@
+pontos = 0
+print ("Bom diaa!")
 
+#Descrição
+
+print("Bem vindo(a) ao Jogo das Perguntas!")
+print("Acerte o máximo de perguntas possíveis!!")
+
+#Perguntas
+
+print("Qual é o seu nome?")
+
+nome = input ()
+
+print("Quantos anos você tem?")
+
+idade = input ()
+
+print ("Legal!!, agora vamos para as perguntas!")
+
+#Jogo das perguntas
+
+print ("Qual é a capital do Brasil?")
+
+capital = input ()
+
+    if capital == brasília:
+        print ("🥳 Parabéns, você acertou")
+       
+       break
+       
+
+print ("Qual é o animal terrestre mais rápido do mundo?")
+
+animal = input ()
+
+    if animal == guepardo:
+       print ("🥳 Parabéns, você acertou")
